9f23811d1017d35ac0a44fad4ef126a7806eb5df
Mohamed Nafih <mdnafih3@gmail.com>
2025-09-04 09:53:47 +0530

    feat(desktop): add start button and dynamic notifications

--- packetscope-desktop/src/main/java/com/packetscope/desktop/MainApp.java
+++ packetscope-desktop/src/main/java/com/packetscope/desktop/MainApp.java
@@ -1,37 +1,64 @@
 package com.packetscope.desktop;
 
+import java.io.File;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.text.*;
 
+public class MainApp extends Application {
 
-
-public class MainApp extends Application{
     @Override
-    public void start(Stage stage) throws Exception{
-        
+    public void start(Stage stage) throws Exception {
+
         // resources
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/primary.fxml"));        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/primary.fxml"));
         Image image = new Image(getClass().getResourceAsStream("/images/packetscopeLogo.jpeg"));
-        
+
         // scene setup
         Parent parent = loader.load();
-        Group root = new Group();
-        Scene scene = new Scene(parent,400,720);
-        scene.getStylesheets().add(getClass().getResource("/styles/primary.css").toExternalForm());
-       
+        Scene scene = new Scene(parent, 400, 720);
+
+        loadStyles(scene);
+
+        // Refresh
+        scene.setOnKeyPressed(event -> {
+            switch (event.getCode()) {
+                case F5 -> {
+                    try {
+                        System.out.println("Reloading scene and CSS...");
+                        Parent newRoot = FXMLLoader.load(getClass().getResource("/fxml/primary.fxml"));
+                        scene.setRoot(newRoot);
+                        loadStyles(scene);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
         // stage setup 
-        stage.setResizable(false); 
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.setTitle("PacketScope Desktop");
         stage.getIcons().add(image);
-        stage.show();   
+        stage.show();
     }
-    
-    public static void main(String args[]){
+
+    private void loadStyles(Scene scene) {
+        scene.getStylesheets().clear();
+        File cssFile = new File("src/main/resources/styles/primary.css");
+        scene.getStylesheets().add(cssFile.toURI().toString());
+
+        scene.getRoot().applyCss();
+        scene.getRoot().layout();
+    }
+
+    public static void main(String args[]) {
         launch();
     }
 }
--- packetscope-desktop/src/main/java/com/packetscope/desktop/controller/PrimaryController.java
+++ packetscope-desktop/src/main/java/com/packetscope/desktop/controller/PrimaryController.java
@@ -1,14 +1,52 @@
 package com.packetscope.desktop.controller;
 
+import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.util.Duration;
 
 public class PrimaryController {
-    
+
     @FXML
-    private void startCapture(ActionEvent e){
-        System.out.println("hellow world");
+    private StackPane rootStack;
+
+    @FXML
+    private StackPane overlayPane;
+
+    @FXML
+    private VBox rootPane;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    Label protectedLabel ;
+
+    private boolean running = false;
+
+    @FXML
+    private void startCapture(ActionEvent e) {
+        if (running) {
+            startButton.setStyle("-fx-border-color: rgb(28,115,232)");
+            startButton.setText("START");
+
+            protectedLabel.setText("UnProtected");
+            protectedLabel.setStyle("-fx-background-color: rgb(255,30,30,0.5)");
+            
+            running = false;
+        } else {
+                      
+            protectedLabel.setText("Protected");
+            protectedLabel.setStyle("-fx-background-color: rgba(0, 225, 121, 0.51)");
+            protectedLabel.setVisible(true);
+            
+            startButton.setStyle("-fx-border-color: rgb(255,62,72)");
+            startButton.setText("STOP");
+
+            running = true;
+        }
     }
 
 }
