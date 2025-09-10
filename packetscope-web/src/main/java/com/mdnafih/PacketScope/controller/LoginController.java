755eb3fae399b02703dcb1d180da87580ebd0d12
Mohamed Nafih <mdnafih3@gmail.com>
2025-09-10 12:23:04 +0530

    chore: remove unused dependencies and dead codes

--- packetscope-web/src/main/java/com/mdnafih/PacketScope/controller/LoginController.java
+++ packetscope-web/src/main/java/com/mdnafih/PacketScope/controller/LoginController.java
@@ -20,34 +20,4 @@ public class LoginController {
         }
         return "login";
     }
-
-//    @GetMapping("/logout")
-//    public String logout(HttpSession session) {
-//        session.invalidate();
-//        return "redirect:/login";
-//    }
-
-//    @PostMapping("/login")
-//    public String processLogin(@RequestParam String username,
-//                               @RequestParam String password,
-//                               HttpSession session,
-//                               Model model) {
-//        if ("admin".equals(username) && "1234".equals(password)) {
-//            session.setAttribute("username", username);
-//            model.addAttribute("message", "Login successful!");
-//            model.addAttribute("user", username);
-//            return "welcome";
-//        } else if ("list".equals(username) && "pass".equals(password)){
-//            session.setAttribute("username", username);
-//            ArrayList<String> garbageList = new ArrayList<>();
-//            IntStream.range(0,5).forEach(i -> garbageList.add("garbage" + i));
-//            model.addAttribute("message", "You have successfully logged in!");
-//            model.addAttribute("user", username);
-//            model.addAttribute("gbList",garbageList);
-//            return "welcome";
-//        } else {
-//            model.addAttribute("error", "Invalid credentials!");
-//            return "login";
-//        }
-//    }
 }
